3cdf71a7bbcf0ae72ad07e93c7ed0e81ea0eb8d6
Majoxs <majoxs@gmail.com>
2021-07-30 20:16:32 +0500

    fix(script.js):fix edits

--- script.js
+++ script.js
@@ -1,21 +1,17 @@
 'use strict';
 
-let money = 1000,
+const money = 1000,
    income = 'Фриланс',
    addExpenses = 'Коммуналка, Питание, Интернет, Развлечения',
    deposit = true,
    mission = 10000,
-   period = 10;
+   period = 10,
+   budgetDay = Math.floor(money / 30);
 
-console.log(typeof money);
-console.log(typeof income);
-console.log(typeof deposit);
+console.log(typeof money, typeof income, typeof deposit);
 console.log(addExpenses.length);
-console.log('Период равен ' + period + ' месяцев. Цель заработать ' + mission + ' долларов.');
+console.log(`Период равен ${period} месяцев. Цель заработать ${mission} долларов.`);
 
-addExpenses = addExpenses.toLowerCase().split(',');
-console.log(addExpenses);
-
-let budgetDay = Math.floor(money / 30);
+console.log(addExpenses.toLowerCase().split(','));
 
 console.log(budgetDay);
