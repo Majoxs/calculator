4347ae4ecf1b4da0ba2a7e4563ad0d9aad137193
Majoxs <majoxs@gmail.com>
2021-08-02 17:10:32 +0500

    feat(script.js): add new functionality on assignment

--- script.js
+++ script.js
@@ -1,17 +1,27 @@
 'use strict';
 
-const money = 1000,
+const money = Number(prompt('Ваш месячный доход?')),
    income = 'Фриланс',
-   addExpenses = 'Коммуналка, Питание, Интернет, Развлечения',
-   deposit = true,
-   mission = 10000,
-   period = 10,
-   budgetDay = Math.floor(money / 30);
+   addExpenses = prompt('Перечислите возможные расходы за рассчитываемый период через запятую'),
+   deposit = confirm('Есть ли у вас депозит в банке?'),
+   mission = 200000,
+   expenses1 = prompt('Введите обязательную статью расходов №1'),
+   amount1 = Number(prompt('Во сколько это обойдётся?')),
+   expenses2 = prompt('Введите обязательную статью расходов №2'),
+   amount2 = Number(prompt('Во сколько это обойдётся?')),
+   budgetMonth = money - (amount1 + amount2),
+   period = Math.ceil(mission / budgetMonth);
+let budgetDay = Math.floor(budgetMonth / 30);
 
 console.log(typeof money, typeof income, typeof deposit);
 console.log(addExpenses.length);
-console.log(`Период равен ${period} месяцев. Цель заработать ${mission} долларов.`);
-
+console.log(`Период равен ${period} месяцев. Цель заработать ${mission} рублей.`);
 console.log(addExpenses.toLowerCase().split(','));
+console.log(`Бюджет на месяц: ${budgetMonth}`);
+console.log(`Цель будет достигнута за ${period} месяцев(-а)`);
+console.log(`Бюджет на день: ${budgetDay}`);
 
-console.log(budgetDay);
+budgetDay = budgetDay >= 1200 ? console.log('У Вас высокий уровень дохода') :
+   budgetDay >= 600 && budgetDay < 1200 ? console.log('У Вас средний уровень дохода') :
+      budgetDay < 600 && budgetDay >= 0 ? console.log('К сожалению у Вас уровень дохода ниже среднего') :
+         console.log('Что-то пошло не так');
